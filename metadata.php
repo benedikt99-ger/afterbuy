69fc011849e6a6a80be5ed2cfe3bbd86d0c7064c
benedikt99-ger <benedikt99@gmail.com>
2026-06-04 11:34:52 +0200

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'nuenemann_afterbuy',
+    'title'       => 'afterbuy – ',
+    'description' => 'Export order for afterbuy',
+    'thumbnail'   => 'pictures/bn_logo.png',
+    'version'     => '5.1.0',
+    'author'      => 'Nünemann',
+    'url'         => 'https://github.com/benedikt99-ger/afterbuy',
+    'email'       => 'benedikt@nuenemann.de',
+    'extend'      => [],
+];
