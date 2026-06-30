5217987c830161409c0b29c5e3259964f1c49d38
benedikt99-ger <benedikt99@gmail.com>
2026-06-30 12:16:09 +0200

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -1,24 +1,21 @@
 <?php
 
 /**
- * Copyright © OXID eSales AG. All rights reserved.
- * See LICENSE file for license details.
+ * Copyright © benedikt nünemann. All rights reserved.
  */
 
-/**
- * Metadata version
- */
+use benedikt99\afterbuy\Module;
+
 $sMetadataVersion = '2.1';
-
 /**
  * Module information
  */
 $aModule = [
-    'id'          => \benedikt99\afterbuy\Module::MODULE_ID,
+    'id'          =>  Module::MODULE_ID,
     'title'       => 'Export Order to afterbuy',
     'description' => 'Export Order to afterbuy',
     'thumbnail'   => 'bn_logo.png',
-    'version'     => '0.2.0',
+    'version'     => '0.2.1',
     'author'      => 'Nünemann',
     'url'         => 'https://github.com/benedikt99-ger/afterbuy',
     'email'       => 'benedikt@nuenemann.de',
