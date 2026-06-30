90cfc501dfb681072bf97bb2e0a2d12f115f21af
benedikt99-ger <benedikt99@gmail.com>
2026-06-30 14:37:56 +0200

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -4,21 +4,23 @@
  * Copyright © benedikt nünemann. All rights reserved.
  */
 
+# use nuenemann\afterbuy\Model\Order as BNOrder;
+
 $sMetadataVersion = '2.1';
 /**
  * Module information
  */
 $aModule = [
-    'id'          => 'benedikt99_afterbuy',
+    'id'          => \nuenemann\afterbuy\Module::MODULE_ID,
     'title'       => 'Export Order to afterbuy',
     'description' => 'Export Order to afterbuy',
     'thumbnail'   => 'bn_logo.png',
-    'version'     => '0.2.1',
+    'version'     => '0.2.3',
     'author'      => 'Nünemann',
     'url'         => 'https://github.com/benedikt99-ger/afterbuy',
     'email'       => 'benedikt@nuenemann.de',
 	'extend' => [
-		\OxidEsales\Eshop\Application\Model\Order::class => \benedikt99\afterbuy\Model\Order::class,
+		\OxidEsales\Eshop\Application\Model\Order::class => \nuenemann\afterbuy\Model\Order::class,
 	],
     'settings' => [
         array('group' => 'afterbuy_api',  'name' => 'sPartnerid',    'type' => 'str', 'value' => ''),
