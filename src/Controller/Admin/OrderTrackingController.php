cf6281304f4f51d2b7e7d037d169999f38372d90
benedikt99-ger <benedikt99@gmail.com>
2026-07-02 11:49:56 +0200

    Update OrderTrackingController.php

--- src/Controller/Admin/OrderTrackingController.php
+++ src/Controller/Admin/OrderTrackingController.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 namespace nuenemann\AfterbuyExport\Controller\Admin;
+
 // use nuenemann\AfterbuyExport\ServiceContainer;
 use OxidEsales\Eshop\Application\Model\Order as EshopOrder;
 use OxidEsales\Eshop\Core\Registry as EshopRegistry;
@@ -12,14 +13,34 @@ class OrderTrackingController extends AdminController
 {
 	// use ServiceContainer;
     protected $_sThisTemplate = '@bn_afterbuy/admin/ordertracking';
+	
     public function render()
     {
+		$result = "@bn_afterbuy/admin/ordertracking";
+		parent::render();
 
-        return parent::render();
+		$orderId = $this->getEditObjectId();
+        if ($orderId) {
+            $order = oxNew(Order::class);
+            if ($order->load($orderId)) {
+                // $afterbuykdnr = $order->oxorder__oxbillnr->value;
+                // $afterbuyuid  = $order->oxorder__oxtrackcode->value;
+
+                // $this->addTplParam('afterbuykdnr', $afterbuykdnr);
+                // $this->addTplParam('afterbuyuid', $afterbuyuid);
+            }
+        }
+        return $result;
+		// return parent::render();
     }
 
     public function saveData(): void
     {
     }
+	
+    public function getOrder(): string
+    {
+    }
+
 
 }
