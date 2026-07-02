b478243196ff36b43236c73c6e6cfc41e4b18de9
benedikt99-ger <benedikt99@gmail.com>
2026-07-02 11:33:17 +0200

    Update OrderTrackingController.php

--- src/Controller/Admin/OrderTrackingController.php
+++ src/Controller/Admin/OrderTrackingController.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Copyright © OXID eSales AG. All rights reserved.
- * See LICENSE file for license details.
- */
-
 declare(strict_types=1);
 
 namespace nuenemann\AfterbuyExport\Controller\Admin;
@@ -13,34 +8,18 @@ use OxidEsales\Eshop\Application\Model\Order as EshopOrder;
 use OxidEsales\Eshop\Core\Registry as EshopRegistry;
 use OxidEsales\Eshop\Application\Controller\Admin\AdminController;
 
-/**
- * @eshopExtension
- *
- * This is an example for a module extension (chain extend) of
- * the shop start controller.
- * NOTE: class must not be final.
- */
 class OrderTrackingController extends AdminController
 {
 	// use ServiceContainer;
     protected $_sThisTemplate = '@bn_afterbuy/admin/afterbuyexport';
     public function render()
     {
-        // $editRequest = $this->getServiceFromContainer(EditRequestInterface::class);
-        // $factsSettings = $this->getServiceFromContainer(FactsSettingsInterface::class);
-        // $this->addTplParam('measurementOptions', $factsSettings->getMeasurementOptions());
-        // $this->addTplParam('additionalInformationOptions', $factsSettings->getAdditionalInformationOptions());
-        // $productFacts = $factsService->getProductFacts($editRequest->getProductId());
-        // $this->addTplParam('nutritionFacts', $productFacts->getNutritionFacts());
+
         return parent::render();
     }
 
     public function saveData(): void
     {
-        // $editRequest = $this->getServiceFromContainer(EditRequestInterface::class);
-        // $productFactsFactory = $this->getServiceFromContainer(ProductFactsFactoryInterface::class);
-        // $factsService = $this->getServiceFromContainer(FactsServiceInterface::class);
-        // $factsService->saveProductFacts($editRequest->getProductId(), $productFactsFactory->getFromRequest());
     }
 
 }
